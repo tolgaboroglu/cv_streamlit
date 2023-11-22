bf3f94d7c75ea8cdcb6de889b48c8b8d581886a4
Tolga Boroglu <tolgaboroglu@gmail.com>
2023-11-22 23:36:15 +0300

    digital_cv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\tolga\cv_streamlit\venv
+set VIRTUAL_ENV=C:\Users\tolga\OneDrive\Belgeler\GitHub\cv_streamlit\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
