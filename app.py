5c2bf2a9da3d5621d3e157d6ab4f53469b08df2a
Tolga Boroglu <tolgaboroglu@gmail.com>
2023-11-26 13:10:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir /"styles/main.css"
-resume_file = current_dir /  "assets/TolgaBoroğlu_resume.pdf"
+resume_file = current_dir /  "assets/tolgaboroglu_cv_.pdf"
 profile_pic = current_dir / "assets/profile-pic.png"
 
 
